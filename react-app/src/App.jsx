11ec3760ce4614268e0573b64fa0af207e1dbe72
austinaeden <austin.mbogo@student.moringaschool.com>
2023-07-23 18:58:23 +0300

    fifth commit

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import SearchBar from './searchBar'
 import NewTransaction from './newtransaction'
+import { useEffect, useState } from 'react'
 
 function FilterableProductTable() {
-  /*const [transaction, setTransactions] = useState([]);
+  const [transaction, setTransactions] = useState([]);
   
   useEffect(() => {
       fetch("http://localhost:3000/transactions")
         .then((r) => r.json())
-        .then(data => setTodos(data.transactions));
+        .then(transactions=>{
+          transactions.forEach((transaction)=>setTransactions(transaction))
+        })
   }, [])
 
   function addTransaction(newTransactions) {
     const updatedTransactions = [...transaction, newTransactions]
     setTransactions(updatedTransactions);
-  }*/
+  }
 
 
   return (
     <div style={{ padding: "5px", border: "1px solid orange"}}>
       <SearchBar />
-      <NewTransaction /*onAddTransaction={addTransaction}*/ />
+      <NewTransaction onAddTransaction={addTransaction} />
     </div>
   )
 }
